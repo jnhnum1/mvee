7fe1dea39b74f483d4818e1c3edf0d9b51403026
Justin Holmgren <justin.holmgren@gmail.com>
2013-02-02 17:58:35 -0500

    using lenses for car step function

--- car.hs
+++ car.hs
@@ -1,8 +1,14 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, TemplateHaskell  #-}
 
 import Control.Lens
+import Control.Lens.TH
 
 import Data.Packed.Vector
+
+-- sigh, most recent version of lens package isn't on hackage
+infixl 1 &
+(&) :: a -> (a -> b) -> b
+a & f = f a
 
 class Vectorizable a s | a -> s where
   toVector :: a -> Vector s
@@ -15,21 +21,27 @@ data Car = Car {_front :: Loc, _back :: Loc, _angle :: Double} deriving (Show)
 makeLenses ''Car
 
 instance Vectorizable Car Double where
-  toVector (Car f b t) = fromList [(x f), (y f), (x b), (y b), t]
+  toVector (Car f b t) = fromList [(_x f), (_y f), (_x b), (_y b), t]
   fromVector v = 
     let
       [xf, yf, xb, yb, t] = toList v
     in Car (Loc xf yf) (Loc xb yb) t
 
 stepCar :: Double -> Double -> Car -> Car
-stepCar v dt (Car f b theta) = 
+stepCar v dt car = 
   let
-    yl = (y f) - (y b)
-    xl = (x f) - (x b)
+    yl = car^.front.y - car^.back.y
+    xl = car^.front.x - car^.back.x
+    theta = car^.angle
     h = sqrt $ yl * yl + xl * xl
     coa = xl / h
     sia = yl / h
     dy = v * ((sin theta) * coa + (cos theta) * sia)
     dx = v * ((cos theta) * coa - (sin theta) * sia)
+    tt = (dx + xl) * coa + (dy + yl) * sia
+    q = dx * coa + xl * coa + dy * sia + yl * sia -
+      0.5 * sqrt (4*tt^2 - 4*(dx^2 + 2*dx*xl + dy^2 + 2*dy*yl))
+  in car & front.x +~ dx & front.y +~ dy & back.x +~ q * coa 
+         & back.y +~ q * sia
 
 main = print $ (Car (Loc 1 2) (Loc 3 4) 5)
